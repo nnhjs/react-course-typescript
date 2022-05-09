2efc38d6b389e74d56dc33a09d2b188f5c622757
nnhungjs <nnhungjs@gmail.com>
2022-05-10 00:12:18 +0700

    props, default props, optional props

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface AppProps {
+  headerText: string;
+  extraText?: string;
 }
 
-export default App;
+export default function App({
+  headerText,
+  extraText = "default text",
+}: AppProps) {
+  return (
+    <>
+      <h1>{headerText}</h1>
+      {extraText && <p>{extraText}</p>}
+    </>
+  );
+}
