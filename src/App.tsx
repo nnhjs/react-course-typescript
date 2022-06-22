1f58680a2569ff9c58104f1fb74e2283b865697d
nnhjs <nnhungjs@gmail.com>
2022-06-22 10:54:47 +0700

    not use enum if you can, if union type instead of A or B or C

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,13 @@
-import { useState } from "react";
+import { useState } from "react"
 
-const defaultFormData = {
-  title: "",
-  body: "",
-};
+
 
 export default function App() {
-  const [formData, setFormData] = useState(defaultFormData);
-  const { title, body } = formData;
-
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setFormData((prevState) => ({
-      ...prevState,
-      [e.target.id]: e.target.value,
-    }));
-  };
-
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    console.log(formData);
-
-    setFormData(defaultFormData);
-  };
-
+  const [checkoutStep, setCheckoutStep] = useState('Details')
   return (
-    <>
-      <h1>Form</h1>
-      <p>Create a post</p>
-
-      <form onSubmit={onSubmit}>
-        <label htmlFor="title">Title</label>
-        <br />
-        <input type="text" id="title" value={title} onChange={onChange} />
-        <br />
-        <br />
-        <label htmlFor="body">Body</label>
-        <br />
-        <input type="text" id="body" value={body} onChange={onChange} />
-        <br />
-        <br />
-        <button type="submit">Upload post</button>
-      </form>
-    </>
-  );
+    <div>{
+      checkoutStep === 'Details' && <h1>Details</h1>
+      <button type="button" onClick={() => } ></button>
+      }</div>
+  )
 }
